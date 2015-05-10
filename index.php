a0f9ac86af066375ceb5d8043fe48f44f8ca2d7d
Andrey O <andrey.orsoev@gmail.com>
2015-05-10 05:19:59 +0000

    Fix

--- index.php
+++ index.php
@@ -23,15 +23,27 @@ $app->get('/', function() use ($app, $db) {
   $app->render('index/index.twig');
 });
 
+$app->get('/my', function() use ($app, $db) {
+    $app->render('index/my.twig');
+});
+
+$app->get('/cashdesk', function() use($app, $db) {
+    $app->render('index/cashdesk.twig');
+});
+
+$app->get('/ajax/greetings', function() use($app, $db) {
+    $app->render('partials/greetings.twig');
+});
+
 $app->post('/api/here', 'API', function() use ($app, $db) {
+    $input = file_get_contents('php://input');
+    $data = json_decode($input, true);
+    
     $res = $db->query("SELECT id, client_id FROM events ORDER BY id DESC LIMIT 1");
     $details = false;
     if ($res) {
         $details = $res->fetch();
     }
-    
-    $input = file_get_contents('php://input');
-    $data = json_decode($input, true);
     
     $current_client_id = !empty($data['client_id']) ? $data['client_id'] : false;
     $client_id = !empty($details['client_id']) ? $details['client_id'] : false;
@@ -56,15 +68,43 @@ $app->post('/api/here', 'API', function() use ($app, $db) {
                 $beacon_id = $res->fetchColumn();
             }
         }
-        
+    
+        $sql = false;    
         if ($beacon_id) {
             $sql = sprintf("INSERT INTO events (client_id, beacon_id, created_at, updated_at) VALUES ('%s', '%d', NOW(), NOW())", $data['client_id'], $beacon_id);    
         }
     }
     
-    $db->exec($sql);
+    if (!empty($sql)) {
+        $db->exec($sql);
+    }
     
-    $app->render(200, array('response' => array('success' => true, 'sql' => $sql)));
+    if ($beacon_id) {
+        $sql = sprintf("UPDATE beacon_to_state SET state_id = %d WHERE beacon_id = %d", 1, $beacon_id);
+        $db->exec($sql);
+    }
+    
+    $app->render(200, array('response' => array('success' => true)));
+});
+
+$app->post("/api/left", 'API', function() use($app, $db) {
+    $input = file_get_contents('php://input');
+    $data = json_decode($input, true);
+    
+    $sql = sprintf("SELECT id FROM beacons WHERE mac = '%s' AND minor = '%s' AND major = '%s'", $data['mac'], $data['minor'], $data['major']);
+    $res = $db->query($sql);
+    
+    $beacon_id = false;
+    if ($res) {
+        $beacon_id = $res->fetchColumn();
+    }    
+   
+    if ($beacon_id) {
+        $sql = sprintf("UPDATE beacon_to_state SET state_id = %d WHERE beacon_id = %d", 2, $beacon_id);
+        $db->exec($sql);
+    }
+    
+    $app->render(200, array('response' => array('success' => true)));
 });
 
 $app->run();
